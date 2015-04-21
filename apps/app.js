3af72cd86496043459668ccb48e4e4ce963deb35
Kevin Schneider <kevin.g.schneider@gmail.com>
2015-04-21 16:11:39 -0400

    initial upload

---
+++ apps/app.js
@@ -0,0 +1,31 @@
+$(function(){
+  $('#search-term').submit(function(event){
+    event.preventDefault();
+    var searchTerm = $('#query').val();
+    getRequest(searchTerm);
+  });
+});
+
+function getRequest(searchTerm){
+  var params = {
+    part: 'snippet',
+    key: "AIzaSyC1rnQgVyuAlDi8xf5ug_9g4XNWeYfhuZ0",
+    q: searchTerm,
+    r: 'json'
+  };
+  
+  url = 'https://www.googleapis.com/youtube/v3/search';
+
+  $.getJSON(url, params, function(data){
+    showResults(data.Search);
+  });
+}
+
+function showResults(results){
+  var html = "";
+  $.each(results, function(index,value){
+    html += '<p>' + value.Title + '</p>';
+    console.log(value.Title);
+  });
+  $('#search-results').html(html);
+}
